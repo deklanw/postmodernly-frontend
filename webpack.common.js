6471bee922bb1612797991e50d4abf72a73f5f5c
Deklan Webster <deklan@gmail.com>
2019-05-26 20:56:10 -0400

    Switch from ts-loader to Babels preset-typescript. Do polyfill properly

--- webpack.common.js
+++ webpack.common.js
@@ -16,7 +16,7 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        use: ['babel-loader', 'ts-loader'],
+        use: ['babel-loader'],
         exclude: /node_modules/
       },
       {
